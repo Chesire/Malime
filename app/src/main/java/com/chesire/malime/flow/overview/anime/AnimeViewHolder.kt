4095dbd22320c652d8b33b396cd267d0d4060e3f
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2019-03-12 17:31:25 +0000

    feat: hide the +1 button if reached total length

--- app/src/main/java/com/chesire/malime/flow/overview/anime/AnimeViewHolder.kt
+++ app/src/main/java/com/chesire/malime/flow/overview/anime/AnimeViewHolder.kt
@@ -4,8 +4,10 @@ import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.chesire.malime.core.models.SeriesModel
+import com.chesire.malime.extensions.visibleIf
 import kotlinx.android.extensions.LayoutContainer
 import kotlinx.android.synthetic.main.adapter_item_anime.adapterItemAnimeImage
+import kotlinx.android.synthetic.main.adapter_item_anime.adapterItemAnimePlusOne
 import kotlinx.android.synthetic.main.adapter_item_anime.adapterItemAnimeTitle
 
 class AnimeViewHolder(view: View) : RecyclerView.ViewHolder(view), LayoutContainer {
@@ -17,5 +19,6 @@ class AnimeViewHolder(view: View) : RecyclerView.ViewHolder(view), LayoutContain
             .load(model.coverImage.largest)
             .into(adapterItemAnimeImage)
         adapterItemAnimeTitle.text = model.title
+        adapterItemAnimePlusOne.visibleIf(invisible = true) { model.progress == model.totalLength }
     }
 }
