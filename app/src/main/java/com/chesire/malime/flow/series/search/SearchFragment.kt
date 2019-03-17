d219711b59116a6ba1577e3787dbc277fec03962
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2019-03-17 16:18:29 +0000

    WIP: add initial files for search

---
+++ app/src/main/java/com/chesire/malime/flow/series/search/SearchFragment.kt
@@ -0,0 +1,6 @@
+package com.chesire.malime.flow.series.search
+
+import dagger.android.support.DaggerFragment
+
+class SearchFragment : DaggerFragment() {
+}
---
+++ app/src/main/java/com/chesire/malime/flow/series/search/SearchViewModel.kt
@@ -0,0 +1,7 @@
+package com.chesire.malime.flow.series.search
+
+import androidx.lifecycle.ViewModel
+import javax.inject.Inject
+
+class SearchViewModel @Inject constructor() : ViewModel() {
+}
