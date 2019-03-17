be6da4a682b513510a136744dbe23635482aaa60
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2019-03-17 15:10:54 +0000

    feat: add initial ui construction

--- app/src/main/java/com/chesire/malime/flow/series/detail/SeriesDetailFragment.kt
+++ app/src/main/java/com/chesire/malime/flow/series/detail/SeriesDetailFragment.kt
@@ -1,6 +1,31 @@
 package com.chesire.malime.flow.series.detail
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.chesire.malime.core.models.SeriesModel
+import com.chesire.malime.databinding.FragmentSeriesDetailBinding
 import dagger.android.support.DaggerFragment
 
 class SeriesDetailFragment : DaggerFragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return FragmentSeriesDetailBinding.inflate(inflater, container, false).root
+    }
+
+    companion object {
+        const val TAG = "SeriesDetailFragment"
+
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @return A new instance of fragment SeriesDetailFragment.
+         */
+        fun newInstance(seriesModel: SeriesModel) = SeriesDetailFragment()
+    }
 }
