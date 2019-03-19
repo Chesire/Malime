c7da0519c93c48fadb7ac3af4788854384201f9b
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2019-03-19 19:53:34 +0000

    style: fix ktlint issue

--- app/src/main/java/com/chesire/malime/flow/series/search/SearchViewModel.kt
+++ app/src/main/java/com/chesire/malime/flow/series/search/SearchViewModel.kt
@@ -3,5 +3,4 @@ package com.chesire.malime.flow.series.search
 import androidx.lifecycle.ViewModel
 import javax.inject.Inject
 
-class SearchViewModel @Inject constructor() : ViewModel() {
-}
+class SearchViewModel @Inject constructor() : ViewModel()
