2d59753a8e0e80675c17d941b39018709fe73c18
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2019-04-13 16:14:43 +0100

    refactor: remove unused code
    Remove the code thats not needed because of the navigation component

--- app/src/main/java/com/chesire/malime/flow/series/list/anime/AnimeFragment.kt
+++ app/src/main/java/com/chesire/malime/flow/series/list/anime/AnimeFragment.kt
@@ -1,6 +1,5 @@
 package com.chesire.malime.flow.series.list.anime
 
-import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -14,7 +13,6 @@ import com.chesire.malime.databinding.FragmentAnimeBinding
 import com.chesire.malime.flow.ViewModelFactory
 import com.chesire.malime.flow.series.list.SeriesListener
 import dagger.android.support.DaggerFragment
-import kotlinx.android.synthetic.main.fragment_anime.fragmentAnimeFab
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -29,15 +27,6 @@ class AnimeFragment : DaggerFragment(), AnimeInteractionListener {
         ViewModelProviders
             .of(this, viewModelFactory)
             .get(AnimeViewModel::class.java)
-    }
-
-    override fun onAttach(context: Context) {
-        super.onAttach(context)
-
-        // if (context !is SeriesListener) {
-        //     throw ClassCastException("Activity must implement SeriesListener")
-        // }
-        // seriesListener = context
     }
 
     override fun onCreateView(
@@ -80,17 +69,5 @@ class AnimeFragment : DaggerFragment(), AnimeInteractionListener {
     override fun onPlusOne(model: SeriesModel) {
         Timber.i("Model ${model.slug} onPlusOne called")
         viewModel.updateSeries(model.userId, model.progress.inc(), model.userSeriesStatus)
-    }
-
-    companion object {
-        const val TAG = "AnimeFragment"
-
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @return A new instance of fragment AnimeFragment.
-         */
-        fun newInstance() = AnimeFragment()
     }
 }
