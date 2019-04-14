595e6e12ac87fdfdfa7c6d427e349ec983f19c58
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2019-04-14 23:47:58 +0100

    feat: add some implementation to syncing
    Add a basic implementation for the syncing screen

--- app/src/main/java/com/chesire/malime/flow/login/syncing/SyncingFragment.kt
+++ app/src/main/java/com/chesire/malime/flow/login/syncing/SyncingFragment.kt
@@ -1,6 +1,60 @@
 package com.chesire.malime.flow.login.syncing
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
+import com.chesire.lifecyklelog.LogLifecykle
+import com.chesire.malime.AsyncState
+import com.chesire.malime.flow.ViewModelFactory
 import dagger.android.support.DaggerFragment
+import javax.inject.Inject
 
+@LogLifecykle
 class SyncingFragment : DaggerFragment() {
+    @Inject
+    lateinit var viewModelFactory: ViewModelFactory
+
+    private val viewModel by lazy {
+        ViewModelProviders
+            .of(this, viewModelFactory)
+            .get(SyncingViewModel::class.java)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return FragmentSyncingBinding
+            .inflate(inflater, container, false)
+            .apply {
+                lifecycleOwner = viewLifecycleOwner
+            }
+            .root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        viewModel.syncStatus.observe(
+            viewLifecycleOwner,
+            Observer {
+                when (it) {
+                    is AsyncState.Success -> {
+                        // navigate out of the screen
+                    }
+                    is AsyncState.Loading -> {
+                        // display loading spinner
+                    }
+                    is AsyncState.Error -> {
+                        // display a retry button
+                    }
+                }
+            }
+        )
+        viewModel.syncLatestData()
+    }
 }
--- app/src/main/java/com/chesire/malime/flow/login/syncing/SyncingViewModel.kt
+++ app/src/main/java/com/chesire/malime/flow/login/syncing/SyncingViewModel.kt
@@ -1,7 +1,50 @@
 package com.chesire.malime.flow.login.syncing
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.chesire.malime.AsyncState
+import com.chesire.malime.IOContext
+import com.chesire.malime.core.Resource
+import com.chesire.malime.extensions.postError
+import com.chesire.malime.extensions.postLoading
+import com.chesire.malime.extensions.postSuccess
+import com.chesire.malime.repo.SeriesRepository
+import com.chesire.malime.repo.UserRepository
+import com.chesire.malime.services.WorkerQueue
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
 import javax.inject.Inject
+import kotlin.coroutines.CoroutineContext
 
-class SyncingViewModel @Inject constructor() : ViewModel() {
+class SyncingViewModel @Inject constructor(
+    private val seriesRepo: SeriesRepository,
+    private val userRepo: UserRepository,
+    private val workerQueue: WorkerQueue,
+    @IOContext private val ioContext: CoroutineContext
+) : ViewModel() {
+
+    private val job = Job()
+    private val ioScope = CoroutineScope(job + ioContext)
+    private val _syncStatus = MutableLiveData<AsyncState<Any, Any>>()
+    val syncStatus: LiveData<AsyncState<Any, Any>>
+        get() = _syncStatus
+
+    fun syncLatestData() = ioScope.launch {
+        _syncStatus.postLoading()
+
+        // should do user as well
+        val syncCommands = listOf(seriesRepo.refreshAnime(), seriesRepo.refreshManga())
+        if (syncCommands.any { it is Resource.Error }) {
+            _syncStatus.postError(Any())
+        } else {
+            _syncStatus.postSuccess(Any())
+        }
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        job.cancel()
+    }
 }
