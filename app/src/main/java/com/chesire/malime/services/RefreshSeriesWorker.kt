cafd2ad7c1ea3c30545f664a162cb694f8d3b880
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2019-04-17 22:54:09 +0100

    fix: only sync the series if logged in
    Check the user repository to see if we are logged in before trying to sync the latest series

--- app/src/main/java/com/chesire/malime/services/RefreshSeriesWorker.kt
+++ app/src/main/java/com/chesire/malime/services/RefreshSeriesWorker.kt
@@ -5,9 +5,8 @@ import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.chesire.malime.MalimeApplication
 import com.chesire.malime.core.Resource
-import com.chesire.malime.injection.components.DaggerAppComponent
 import com.chesire.malime.repo.SeriesRepository
-import kotlinx.coroutines.Dispatchers
+import com.chesire.malime.repo.UserRepository
 import timber.log.Timber
 import javax.inject.Inject
 
@@ -17,7 +16,9 @@ class RefreshSeriesWorker(
 ) : CoroutineWorker(appContext, workerParams) {
 
     @Inject
-    lateinit var repo: SeriesRepository
+    lateinit var seriesRepo: SeriesRepository
+    @Inject
+    lateinit var userRepo: UserRepository
 
     init {
         // For now setup in the init block
@@ -31,7 +32,18 @@ class RefreshSeriesWorker(
     override suspend fun doWork(): Result {
         Timber.i("doWork RefreshSeriesWorker")
 
-        return if (listOf(repo.refreshAnime(), repo.refreshManga()).any { it is Resource.Error }) {
+        if (userRepo.retrieveUserId() == null) {
+            Timber.i("doWork no userId found, so cancelling")
+            return Result.success()
+        }
+
+        Timber.i("doWork userId found, beginning to refresh")
+        return if (
+            listOf(
+                seriesRepo.refreshAnime(),
+                seriesRepo.refreshManga()
+            ).any { it is Resource.Error }
+        ) {
             Result.retry()
         } else {
             Result.success()
