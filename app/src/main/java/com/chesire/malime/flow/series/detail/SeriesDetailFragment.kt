58050b3e576753afc264ece085f0be8be69e713b
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2019-04-25 12:52:36 +0100

    feat: add additional layout to detail view

--- app/src/main/java/com/chesire/malime/flow/series/detail/SeriesDetailFragment.kt
+++ app/src/main/java/com/chesire/malime/flow/series/detail/SeriesDetailFragment.kt
@@ -2,9 +2,12 @@ package com.chesire.malime.flow.series.detail
 
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.transition.TransitionInflater
 import com.bumptech.glide.Glide
@@ -14,6 +17,7 @@ import com.chesire.malime.flow.ViewModelFactory
 import dagger.android.support.DaggerFragment
 import kotlinx.android.synthetic.main.fragment_series_detail.fragmentSeriesDetailHeaderImage
 import kotlinx.android.synthetic.main.fragment_series_detail.fragmentSeriesDetailImageView
+import kotlinx.android.synthetic.main.fragment_series_detail.fragmentSeriesDetailToolbar
 import javax.inject.Inject
 
 @LogLifecykle
@@ -33,23 +37,27 @@ class SeriesDetailFragment : DaggerFragment() {
 
         sharedElementEnterTransition =
             TransitionInflater.from(context).inflateTransition(android.R.transition.move)
+        setHasOptionsMenu(true)
     }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        return FragmentSeriesDetailBinding.inflate(inflater, container, false)
-            .apply {
-                vm = viewModel
-                lifecycleOwner = viewLifecycleOwner
-            }
-            .root
-    }
+    ) = FragmentSeriesDetailBinding.inflate(inflater, container, false)
+        .apply {
+            vm = viewModel
+            lifecycleOwner = viewLifecycleOwner
+        }
+        .root
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        (activity as? AppCompatActivity)?.let { activity ->
+            activity.setSupportActionBar(fragmentSeriesDetailToolbar)
+            activity.supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        }
 
         fragmentSeriesDetailImageView.transitionName = args.series.title
         Glide.with(this)
@@ -63,5 +71,12 @@ class SeriesDetailFragment : DaggerFragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel.model = args.series
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> findNavController().navigateUp()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
