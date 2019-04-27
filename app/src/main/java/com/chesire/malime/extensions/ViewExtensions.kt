2985e6294a7f4c1b700241446bb6bda90876c884
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2019-04-27 23:00:49 +0100

    feat: hide the bottom nav in select views
    When in the inner views hide the bottom navigation bar

--- app/src/main/java/com/chesire/malime/extensions/ViewExtensions.kt
+++ app/src/main/java/com/chesire/malime/extensions/ViewExtensions.kt
@@ -2,6 +2,14 @@ package com.chesire.malime.extensions
 
 import android.view.View
 
+fun View.hide(invisible: Boolean = false) {
+    visibility = if (invisible) View.INVISIBLE else View.GONE
+}
+
+fun View.show() {
+    visibility = View.VISIBLE
+}
+
 fun View.visibleIf(invisible: Boolean = false, callback: () -> Boolean) {
     visibility = if (callback()) {
         View.VISIBLE
