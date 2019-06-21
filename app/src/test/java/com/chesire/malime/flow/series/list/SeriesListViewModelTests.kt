8ff5c26ff77a7f71fea3e879376a9d46e33a391b
Troy Rijkaard <troyrijkaard@hotmail.co.uk>
2019-06-21 21:36:05 +0100

    test: update the serieslist tests
    Update tests for the SeriesListViewModel

--- app/src/test/java/com/chesire/malime/flow/series/list/SeriesListViewModelTests.kt
+++ app/src/test/java/com/chesire/malime/flow/series/list/SeriesListViewModelTests.kt
@@ -5,6 +5,7 @@ import com.chesire.malime.AuthCaster
 import com.chesire.malime.CoroutinesMainDispatcherRule
 import com.chesire.malime.core.Resource
 import com.chesire.malime.core.flags.UserSeriesStatus
+import com.chesire.malime.core.models.SeriesModel
 import com.chesire.malime.repo.SeriesRepository
 import io.mockk.Runs
 import io.mockk.coEvery
@@ -13,6 +14,7 @@ import io.mockk.every
 import io.mockk.just
 import io.mockk.mockk
 import io.mockk.verify
+import org.junit.Assert.assertTrue
 import org.junit.Rule
 import org.junit.Test
 
@@ -36,7 +38,7 @@ class SeriesListViewModelTests {
         val mockAuthCaster = mockk<AuthCaster>()
 
         val classUnderTest = SeriesListViewModel(mockRepo, mockAuthCaster)
-        classUnderTest.updateSeries(0, 0, UserSeriesStatus.Current)
+        classUnderTest.updateSeries(0, 0, UserSeriesStatus.Current, { })
 
         coVerify { mockRepo.updateSeries(0, 0, UserSeriesStatus.Current) }
     }
@@ -57,8 +59,48 @@ class SeriesListViewModelTests {
         }
 
         val classUnderTest = SeriesListViewModel(mockRepo, mockAuthCaster)
-        classUnderTest.updateSeries(0, 0, UserSeriesStatus.Current)
+        classUnderTest.updateSeries(0, 0, UserSeriesStatus.Current, { })
 
         verify { mockAuthCaster.issueRefreshingToken() }
+    }
+
+    @Test
+    fun `updateSeries failure not 401 invokes callback`() {
+        var condition = false
+        val mockRepo = mockk<SeriesRepository> {
+            coEvery {
+                updateSeries(0, 0, UserSeriesStatus.Current)
+            } coAnswers {
+                Resource.Error("error", Resource.Error.GenericError)
+            }
+            every { anime } returns mockk()
+            every { manga } returns mockk()
+        }
+        val mockAuthCaster = mockk<AuthCaster>()
+
+        val classUnderTest = SeriesListViewModel(mockRepo, mockAuthCaster)
+        classUnderTest.updateSeries(0, 0, UserSeriesStatus.Current) { condition = true }
+
+        assertTrue(condition)
+    }
+
+    @Test
+    fun `updateSeries success invokes callback`() {
+        var condition = false
+        val mockRepo = mockk<SeriesRepository> {
+            coEvery {
+                updateSeries(0, 0, UserSeriesStatus.Current)
+            } coAnswers {
+                Resource.Success(mockk())
+            }
+            every { anime } returns mockk()
+            every { manga } returns mockk()
+        }
+        val mockAuthCaster = mockk<AuthCaster>()
+
+        val classUnderTest = SeriesListViewModel(mockRepo, mockAuthCaster)
+        classUnderTest.updateSeries(0, 0, UserSeriesStatus.Current) { condition = true }
+
+        assertTrue(condition)
     }
 }
